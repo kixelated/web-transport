5f8c3312a826b1f337e189459d7f5fb0c5c322d8
Luke Curley <kixelated@gmail.com>
2025-09-09 17:33:51 -0700

    Minor rewording.

--- web-transport-quinn/src/recv.rs
+++ web-transport-quinn/src/recv.rs
@@ -80,8 +80,8 @@ impl RecvStream {
     /// > **Warning**
     /// >
     /// > WebTransport sessions share the QUIC connection with HTTP/3 and potentially other sessions.
-    /// > The [quinn::StreamId::index] might not increment by 1 like expected for normal QUIC streams.
-    /// > As a result, the Javascript WebTransport API does not expose the Stream ID on purpose.
+    /// > The [quinn::StreamId::index] might not increment by 1 like expected when using [quinn].
+    /// > This is why the Javascript WebTransport API does not expose the Stream ID.
     pub fn quic_id(&self) -> quinn::StreamId {
         self.inner.id()
     }
