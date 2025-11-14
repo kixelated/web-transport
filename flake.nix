031c93f7e25afbf81a15e58aae9472b63e9dffaf
Luke Curley <kixelated@gmail.com>
2025-11-13 17:31:26 -0800

    Try using the boring crate to see if it fixes CI.

--- flake.nix
+++ flake.nix
@@ -30,17 +30,11 @@
           pkgs.pkg-config
           pkgs.glib
           pkgs.gtk3
-          pkgs.stdenv.cc.cc.lib
-          pkgs.libffi
         ];
       in
       {
         devShells.default = pkgs.mkShell {
           packages = tools;
-
-          shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc.lib pkgs.libffi]}:$LD_LIBRARY_PATH
-          '';
         };
       }
     );
